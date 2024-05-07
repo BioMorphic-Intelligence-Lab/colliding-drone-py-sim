1f26f0fde310059d0e5b82c8071d06527d66e87d
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2024-05-06 18:02:20 -0700

    Adjust limits and add speed_factor for animation

--- animate_traj.py
+++ animate_traj.py
@@ -3,27 +3,31 @@ import numpy as np
 import subprocess
 from tensegrity_drone import TensegrityDrone
 
-def animate(t, traj, name="video.mp4"):
-    drone = TensegrityDrone(plot=True)
+def animate(t, traj,
+            drone,
+            name="video.mp4",
+            speed_factor=1.0):
     
     # Read out the limits for plotting
     lower_limit = min(traj[:,0:3].flatten()) - 0.1
     upper_limit = max(traj[:,0:3].flatten()) + 0.1
+    limit = max(np.abs(lower_limit), np.abs(upper_limit))
 
     # Loop through the traj and save a frame for each point
-    print("Drawing frames of the traj... \n")
+    print("Drawing frames of the traj...")
     for i in tqdm(range(len(t))):
         drone.set_pose(traj[i, :])
         drone.plot_tensegrity_drone(t[i])
-        drone.set_limits(xlim=(lower_limit, upper_limit),
-                         ylim=(lower_limit, upper_limit),
-                         zlim=(lower_limit, upper_limit))
+        drone.set_limits(xlim=(-limit, limit),
+                         ylim=(-limit, limit),
+                         zlim=(-limit, limit))
         drone.save(f"./frames/frame{i:02}.png")
     
-    print("\n... done!")
+    print("... done!")
 
     # Call ffmpeg to animate the video
-    command =  ["ffmpeg", "-f", "image2", "-framerate", f"{len(t) / t[-1]}",
+    command =  ["ffmpeg", "-f", "image2", "-framerate",
+                f"{speed_factor * len(t) / t[-1]}",
                 "-i", "frames/frame%02d.png", "-vcodec",
                 "libx264", "-crf", "22", f"{name}"]
     subprocess.run(command)
